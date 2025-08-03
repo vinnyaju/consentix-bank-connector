dd95667497671dbbe2a1ef04e005e0f665c9a72e
Marcus Vinicius R. Andrade <vinnyaju@gmail.com>
2025-08-03 15:58:50 -0300

    Renomeando o projeto.

--- src/main/java/dev/consentix/openfinance/BankConnectorDcrApplication.java
+++ src/main/java/dev/consentix/openfinance/BankConnectorDcrApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankConnectorApplication {
+public class BankConnectorDcrApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankConnectorApplication.class, args);
+		SpringApplication.run(BankConnectorDcrApplication.class, args);
 	}
 
 }
